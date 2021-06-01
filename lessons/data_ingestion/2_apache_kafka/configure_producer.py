8c0857929cac735bec035b2f779216c1d4a602d4
Fitzy <fitzystrikesagain@gmail.com>
2021-06-01 18:15:00 -0500

    start "Workspace Exercise: Configure a Producer"

--- lessons/data_ingestion/2_apache_kafka/configure_producer.py
+++ lessons/data_ingestion/2_apache_kafka/configure_producer.py
@@ -1,11 +1,14 @@
-from datetime import datetime
-from dataclasses import dataclass, field, asdict
+# Please complete the TODO items in the code.
+
+import asyncio
+from dataclasses import dataclass, field
 import json
 import random
 
 from confluent_kafka import Consumer, Producer
 from confluent_kafka.admin import AdminClient, NewTopic
 from faker import Faker
+
 
 faker = Faker()
 
@@ -21,31 +24,54 @@ class Purchase:
 
     def serialize(self):
         """Serializes the object in JSON string format"""
-        return json.dumps(asdict(self))
+        # TODO: Serializer the Purchase object
+        #       See: https://docs.python.org/3/library/json.html#json.dumps
+        return ""
 
 
-def produce_sync(topic_name):
+async def produce_sync(topic_name):
     """Produces data synchronously into the Kafka Topic"""
     p = Producer({"bootstrap.servers": BROKER_URL})
-    start_time = datetime.utcnow()
-    curr_iteration = 0
 
-    while curr_iteration <= 10000:
-        p.produce(topic_name, Purchase().serialize())
-        p.flush()
-        if curr_iteration % 1000 == 0:
-            elapsed = (datetime.utcnow() - start_time).seconds
-            print(f"Msgs sent: {curr_iteration} | Seconds elapsed: {elapsed}")
-        curr_iteration += 1
+    # TODO: Write a synchronous production loop.
+    #       See: https://docs.confluent.io/current/clients/confluent-kafka-python/#confluent_kafka.Producer.flush
+    while True:
+        # TODO: Instantiate a `Purchase` on every iteration. Make sure to serialize it before
+        #       sending it to Kafka!
+        # Do not delete this!
+        await asyncio.sleep(0.01)
 
 
 def main():
     """Checks for topic and creates the topic if it does not exist"""
     create_topic(TOPIC_NAME)
     try:
-        produce_sync(TOPIC_NAME)
+        asyncio.run(produce_consume())
     except KeyboardInterrupt as e:
         print("shutting down")
+
+
+async def produce_consume():
+    """Runs the Producer and Consumer tasks"""
+    t1 = asyncio.create_task(produce_sync(TOPIC_NAME))
+    t2 = asyncio.create_task(_consume(TOPIC_NAME))
+    await t1
+    await t2
+
+
+async def _consume(topic_name):
+    """Consumes produced messages"""
+    c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0"})
+    c.subscribe([topic_name])
+    num_consumed=0
+    while True:
+        msg = c.consume(timeout=0.001)
+        if msg:
+            num_consumed += 1
+            if num_consumed % 100 == 0:
+                print(f"consumed {num_consumed} messages")
+        else:
+            await asyncio.sleep(0.01)
 
 
 def create_topic(client):
@@ -58,7 +84,7 @@ def create_topic(client):
         try:
             future.result()
         except Exception as e:
-            pass
+            print("exiting production loop")
 
 
 if __name__ == "__main__":
