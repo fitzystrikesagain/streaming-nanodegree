ab8aeccea46ac4137542251f398a6ad7e312a801
Fitzy <fitzystrikesagain@gmail.com>
2021-06-02 01:57:41 -0500

    finish "Build a Kafka Consumer" lesson

--- lessons/data_ingestion/2_apache_kafka/build_a_consumer.py
+++ lessons/data_ingestion/2_apache_kafka/build_a_consumer.py
@@ -1,5 +1,3 @@
-# Please complete the TODO items in the code
-
 import asyncio
 from dataclasses import dataclass, field
 import json
@@ -9,26 +7,46 @@ from confluent_kafka import Consumer, Producer
 from confluent_kafka.admin import AdminClient, NewTopic
 from faker import Faker
 
-
 faker = Faker()
 
 BROKER_URL = "PLAINTEXT://localhost:9092"
 
 
-async def consume(topic_name):
-    """Consumes data from the Kafka Topic"""
+async def poll(topic_name):
+    """Consumes data from the Kafka Topic using the `poll` method"""
     c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0"})
     c.subscribe([topic_name])
 
     while True:
-        #
-        # TODO: Write a loop that uses consume to grab 5 messages at a time and has a timeout.
-        #       See: https://docs.confluent.io/current/clients/confluent-kafka-python/index.html?highlight=partition#confluent_kafka.Consumer.consume
-        #
+        message = c.poll(timeout=1.0)
+        if not message:
+            print("No message received")
+            continue  # No data was retrieved
+        elif message.error() is not None:
+            print(f"Received the following error: {message.error()}")
+            continue  # Log error in production
+        else:
+            print(message.key(), message.value())
 
-        # TODO: Print something to indicate how many messages you've consumed. Print the key and value of
-        #       any message(s) you consumed
+        # Do not delete this!
+        await asyncio.sleep(0.01)
 
+
+async def consume(topic_name):
+    """Consumes data from the Kafka Topic using the consume method"""
+    c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0"})
+    c.subscribe([topic_name])
+    while True:
+        messages = c.consume(num_messages=10, timeout=1.0)
+        for message in messages:
+            if not message:
+                print("No message received")
+                continue  # No data was retrieved
+            elif message.error() is not None:
+                print(f"Received the following error: {message.error()}")
+                continue  # Log error in production
+            else:
+                print(message.key(), message.value())
         # Do not delete this!
         await asyncio.sleep(0.01)
 
