d5175134e67cc88949db796ac3f259f6c2842140
Fitzy <fitzystrikesagain@gmail.com>
2021-06-11 16:43:03 -0500

    restructure project

--- lessons/data_ingestion/utils/constants.py
+++ lessons/data_ingestion/utils/constants.py
@@ -1,4 +1,5 @@
 # Kafka Connect API
 CONNECT_BASE_URL = "http://localhost:8083"
+BROKER_URL = "http://localhost:9092"
 CONNECT_HEADERS = {"Accept": "application/json", "Content-Type": "application/json"}
 
