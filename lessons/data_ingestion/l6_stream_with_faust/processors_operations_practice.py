50d1f39304ab39750c850bf98e5f8f5259a5604b
Fitzy <fitzystrikesagain@gmail.com>
2021-06-20 23:33:30 -0500

    finish Processors and Operations

--- lessons/data_ingestion/l6_stream_with_faust/processors_operations_practice.py
+++ lessons/data_ingestion/l6_stream_with_faust/processors_operations_practice.py
@@ -1,33 +1,35 @@
-# Please complete the TODO items in the code
-
-from dataclasses import asdict, dataclass
-import json
 import random
+from abc import ABC
+from dataclasses import dataclass
 
 import faust
 
+IN_TOPIC = "com.udacity.streams.clickevents"
+OUT_TOPIC = "com.udacity.streams.clickevents.scored"
+
 
 @dataclass
-class ClickEvent(faust.Record):
+class ClickEvent(faust.Record, ABC):
     email: str
     timestamp: str
     uri: str
     number: int
-    score: int = 0
+    fraud_score: float = 0.0
+    fraud_likely: bool = False
 
 
-#
-# TODO: Define a scoring function for incoming ClickEvents.
-#       It doens't matter _how_ you score the incoming records, just perform
-#       some modification of the `ClickEvent.score` field and return the value
-#
-# def add_score(...):
+def add_score(click):
+    fraud_score = round(random.random(), 2)
+    click.fraud_score = fraud_score
+    if fraud_score > .8:
+        click.fraud_likely = True
+    return click
 
 
 app = faust.App("exercise5", broker="kafka://localhost:9092")
-clickevents_topic = app.topic("com.udacity.streams.clickevents", value_type=ClickEvent)
-scored_topic = app.topic(
-    "com.udacity.streams.clickevents.scored",
+clickevents_topic = app.topic(IN_TOPIC, value_type=ClickEvent)
+fraud_scored_topic = app.topic(
+    OUT_TOPIC,
     key_type=str,
     value_type=ClickEvent,
 )
@@ -35,13 +37,9 @@ scored_topic = app.topic(
 
 @app.agent(clickevents_topic)
 async def clickevent(clickevents):
-    #
-    # TODO: Add the `add_score` processor to the incoming clickevents
-    #       See: https://faust.readthedocs.io/en/latest/reference/faust.streams.html?highlight=add_processor#faust.streams.Stream.add_processor
-    #
-
+    clickevents.add_processor(add_score)
     async for ce in clickevents:
-        await scored_topic.send(key=ce.uri, value=ce)
+        await fraud_scored_topic.send(key=ce.uri, value=ce)
 
 
 if __name__ == "__main__":
