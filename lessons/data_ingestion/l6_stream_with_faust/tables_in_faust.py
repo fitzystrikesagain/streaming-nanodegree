65146212570e84bcad9285b107850641b00d56de
Fitzy <fitzystrikesagain@gmail.com>
2021-06-21 00:29:12 -0500

    finish Tables in Faust

--- lessons/data_ingestion/l6_stream_with_faust/tables_in_faust.py
+++ lessons/data_ingestion/l6_stream_with_faust/tables_in_faust.py
@@ -1,12 +1,11 @@
-# Please complete the TODO items in the code
-
+from abc import ABC
 from dataclasses import dataclass
 
 import faust
 
 
 @dataclass
-class ClickEvent(faust.Record):
+class ClickEvent(faust.Record, ABC):
     email: str
     timestamp: str
     uri: str
@@ -16,27 +15,14 @@ class ClickEvent(faust.Record):
 app = faust.App("exercise6", broker="kafka://localhost:9092")
 clickevents_topic = app.topic("com.udacity.streams.clickevents", value_type=ClickEvent)
 
-
-#
-# TODO: Define a uri summary table
-#       See: https://faust.readthedocs.io/en/latest/userguide/tables.html#basics
-#
-# uri_summary_table = app.Table(...)
+uri_summary_table = app.Table("uri-summary", default=int)
 
 
 @app.agent(clickevents_topic)
 async def clickevent(clickevents):
-    #
-    # TODO: Group By URI
-    #       See: https://faust.readthedocs.io/en/latest/userguide/streams.html#group-by-repartition-the-stream
-    #
-    async for ce in clickevents:
-        #
-        # TODO: Use the URI as key, and add the number for each click event. Print the updated
-        #       entry for each key so you can see how the table is changing.
-        #       See: https://faust.readthedocs.io/en/latest/userguide/tables.html#basics
-        #
-        pass
+    async for ce in clickevents.group_by(ClickEvent.uri):
+        uri_summary_table[ce.uri] += ce.number
+        print(f"{ce.uri}: {uri_summary_table[ce.number]}")
 
 
 if __name__ == "__main__":
